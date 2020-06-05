fe34f31cb9b169728371d9dff6f0f133190bc0e3
Sarath Kumar Barathi <beyescay@Saraths-MacBook-Pro.local>
2020-06-05 16:26:51 -0500

    Add max sub array sum solution

---
+++ 4_max_sub_array.cpp
@@ -0,0 +1,30 @@
+// Find max sum of sub array
+
+#include <vector>
+#include <iostream>
+
+using std::vector;
+
+int MaxSubArray(vector<int>& nums) {
+
+  int max_sum = nums[0];
+
+  for(int i = 0, curr_sum = 0; i < nums.size(); ++i) {
+
+      curr_sum += nums[i];
+
+      curr_sum = curr_sum > nums[i] ? curr_sum : nums[i];
+      max_sum = curr_sum > max_sum ? curr_sum : max_sum;
+
+  }
+
+  return max_sum;
+
+}
+
+int main() {
+
+  vector<int> nums{-2,1,-3,4,-1,2,1,-5,4};
+
+  std::cout << MaxSubArray(nums) << std::endl;
+}
